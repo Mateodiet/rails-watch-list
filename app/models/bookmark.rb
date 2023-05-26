7ab0eee65df2d01b57bf4eb52896d15183a65c12
Mateo Diet <mat.diet@orange.fr>
2023-05-26 15:04:09 +0200

    Rails watchlist day 2

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,8 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates :comment, length: { minimum: 6 }
+  validates :comment, length: { minimum: 6 }, presence: true
   validates :list, uniqueness: { scope: :movie }
 end
-
-# validates_associated :books
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show', as: 'list'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new'
+  # post 'lists/:list_id/bookmarks', to: 'bookmarks#create'
+
+
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
+  delete 'lists/:id', to: 'lists#destroy', as: 'list_destroy'
 end
